f538de764a0b9ae762a9f0cf168e438772de10b3
Mattis Mason <Mattissmiles@gmail.com>
2023-01-27 10:44:47 +1100

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Mattis Mason  on 27/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
